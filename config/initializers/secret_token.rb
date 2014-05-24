27e5af426d32d1823cbd4707816fa0eba4d23e6b
Travis Peterson <tekman82@yahoo.com>
2014-05-24 02:43:46 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tbecommerce::Application.config.secret_key_base = '4c523e1becd8bc6349137fbcb3419ccc2ecd281c7e9cacfa16ac7923ac09e11c78c8e3816d665939e12d8aea87798dc8b338e2939bb52bbdfbd3ec17f443fe79'
