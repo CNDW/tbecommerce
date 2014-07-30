6c717ca6defc89044a227f7f243b23c8937aedea
Travis Peterson <tekman82@yahoo.com>
2014-07-29 19:38:23 -0500

    fixes paperclip attachment error

--- app/models/spree/option_value_decorator.rb
+++ app/models/spree/option_value_decorator.rb
@@ -1,9 +1,10 @@
 module Spree
   OptionValue.class_eval do
     has_attached_file :option_image,
-      styles: { medium: "300x300>", thumb: "100x100>", large: "700x700>" },
+      styles: { medium: "300x300>", thumb: "100x100>", large: "600x600>" },
       default_style: :medium,
-      url: "/spree/option_types/:option_type_id/option_values/:id/:style/:option_image.:extension",
+      url: '/spree/option_types/:id/:style/:basename.:extension',
+      path: ':rails_root/public/spree/option_types/:id/:style/:basename.:extension',
       convert_options: { all: '-strip -auto-orient -colorspace sRGB' }
     validates_attachment :option_image,
       content_type: { content_type: %w(image/jpeg image/jpg image/png image/gif) }
--- app/models/spree/product_property_decorator.rb
+++ app/models/spree/product_property_decorator.rb
@@ -3,8 +3,10 @@ module Spree
     has_attached_file :property_image,
       styles: { medium: "300x300>", thumb: "100x100>", large: "700x700>" },
       default_style: :medium,
-      url: "/spree/products/:product_id/product_properties/:id/:style/property_image.:extension",
+      url: '/spree/products/:id/product_properties/:id/:style/:basename.:extension',
+      path: ':rails_root/public/spree/products/:id/product_properties/:id/:style/:basename.:extension',
       convert_options: { all: '-strip -auto-orient -colorspace sRGB' }
+      # url: "/spree/products/:product_id/product_properties/:id/:style/property_image.:extension"
     validates_attachment :property_image,
       content_type: { :content_type => %w(image/jpeg image/jpg image/png image/gif) }
 
