770adea10cb9d561d67a9a7a664785f13cc2e657
Travis Peterson <travis.peterson@ave81.com>
2016-05-17 19:25:34 -0500

    deploy fix

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -5,4 +5,4 @@
 # Mime::Type.register_alias "text/html", :iphone
 svg_raw = MIME::Types["text/html"].first
 svg_raw.extensions << "svg"
-MIME::Types.index_extensions! svg_raw
+MIME::Types.add svg_raw
